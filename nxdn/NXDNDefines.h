a65c44a8df1867517f9a30346577d22ba2797b19
Bryan Biedenkapp <gatekeep@gmail.com>
2025-11-06 15:28:23 -0500

    [NO FUNCTIONAL CHANGE] cleanup file headers containing lingering old comments;

--- nxdn/NXDNDefines.h
+++ nxdn/NXDNDefines.h
@@ -3,11 +3,7 @@
 * Digital Voice Modem - Modem Firmware
 * GPLv2 Open Source. Use is subject to license terms.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-*
-* @package DVM / Modem Firmware
-* @derivedfrom MMDVM (https://github.com/g4klx/MMDVM)
-* @license GPLv2 License (https://opensource.org/licenses/GPL-2.0)
-*
+
 *  Copyright (C) 2016,2017,2018 Jonathan Naylor, G4KLX
 *
 */
